68bc01403c6ba063af5ffc5a5cfaffaacfda03bc
Luke Evans <levans002@gmail.com>
2012-10-31 16:02:36 -0600

    depth 3

--- src/cs555/crawler/utilities/Constants.java
+++ src/cs555/crawler/utilities/Constants.java
@@ -27,7 +27,7 @@ public class Constants {
 	public static final int Failure = 99;
 	public static final int Success = 100;
 	
-	public static final int depth = 2;
+	public static final int depth = 3;
 	
 	
 	// Message sizes
